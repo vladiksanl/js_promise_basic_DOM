a92bddf315606ecbc1a1d5d924ee024c93e55dfb
vladiksanl <vladislav.liashenko1@gmail.com>
2026-01-20 14:08:06 +0200

    solved2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,32 +13,28 @@ rejMes.textContent = 'Promise was rejected!';
 resMes.classList.add('message');
 rejMes.classList.add('message', 'error-message');
 
-function messages() {
-  const p1 = new Promise((resolve) => {
-    logo.addEventListener('click', (e) => {
-      resolve();
-    });
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', (e) => {
+    resolve();
+  });
+})
+  .then(() => {
+    body.append(resMes.cloneNode(true));
   })
-    .then(() => {
-      body.append(resMes.cloneNode(true));
-    })
-    .catch((e) => {
-      body.append(rejMes.cloneNode(true));
-    });
+  .catch(() => {
+    body.append(rejMes.cloneNode(true));
+  });
 
-  const p2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject(new Error('Promise was rejected!'));
-    }, 3000);
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+})
+  .then(() => {
+    body.append(resMes.cloneNode(true));
   })
-    .then(() => {
-      body.append(resMes.cloneNode(true));
-    })
-    .catch((e) => {
-      body.append(rejMes.cloneNode(true));
-    });
+  .catch(() => {
+    body.append(rejMes.cloneNode(true));
+  });
 
-  return [p1, p2];
-}
-
-messages();
+Promise.allSettled([promise1, promise2]).then(() => {});
